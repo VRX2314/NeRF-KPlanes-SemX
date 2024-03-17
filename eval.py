97634dbd6cb31058b211421b0a43f6f3b6c4eae1
VRX2314 <vanshgalaxy@gmail.com>
2024-03-17 14:56:46 +0530

    evaluate script

--- eval.py
+++ eval.py
@@ -3,14 +3,22 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+from skimage.metrics import (
+    peak_signal_noise_ratio,
+    mean_squared_error,
+    structural_similarity,
+)
+from skimage.color import rgb2gray
+
 from backbones.nerf import NerfModel
 from render import render_rays
 
 
 @torch.no_grad()
-def test(hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400):
+def eval(hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400):
     ray_origins = dataset[img_index * H * W : (img_index + 1) * H * W, :3]
     ray_directions = dataset[img_index * H * W : (img_index + 1) * H * W, 3:6]
+    target = dataset[img_index * H * W : (img_index + 1) * H * W, 6:]
 
     data = []
     for i in tqdm(
@@ -31,10 +39,21 @@ def test(hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400)
         data.append(regenerated_px_values)
 
     img = torch.cat(data).data.cpu().numpy().reshape(H, W, 3)
-    plt.figure()
-    plt.imshow(img)
-    plt.savefig(f"./inferences/{img_index}.png", bbox_inches="tight")
-    plt.close()
+    target_img = target.reshape(400, 400, 3)
+    target_img = target_img.detach().cpu().numpy()
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(img)
+    ax[0].set_title("Synthesized")
+    ax[1].imshow(target_img)
+    ax[1].set_title("Actual")
+
+    psnr = peak_signal_noise_ratio(target_img, img)
+    mse = mean_squared_error(target_img, img)
+    ssim = structural_similarity(rgb2gray(target_img), rgb2gray(img), data_range=255)
+
+    fig.suptitle(f"PSNR: {psnr}\nMSE: {mse}\nSSIM: {ssim}")
+
+    plt.savefig(f"./metric_inferences/{img_index}.png", bbox_inches="tight")
 
 
 if __name__ == "__main__":
@@ -47,4 +66,4 @@ if __name__ == "__main__":
     )
 
     for img_index in range(200):
-        test(2, 6, testing_dataset, img_index=img_index, nb_bins=192, H=400, W=400)
+        eval(2, 6, testing_dataset, img_index=img_index, nb_bins=192, H=400, W=400)
